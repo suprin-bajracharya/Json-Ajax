08b3c36b01bf3d77a48942a7bfa72a9e19eb4265
suprin-bajracharya <bajrasurpin@gmail.com>
2016-11-23 14:09:03 +0545

    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var myData = {
+	"name" : "Drogoz",
+	"species" : "dragon",
+	"primaryWeapon" : "cannon"
+
+}
+
+var myWeapons = ["Galil", "Klachnikov", "H&K mp5", "M4A1Carbine"];
+
+var theSoldiers = [
+	{
+		"name" : "Drogoz",
+		"species" : "dragon",
+		"primaryWeapon" : "cannon"
+
+	},
+	{
+		"name" : "Viktor",
+		"species" : "Human",
+		"primaryWeapon" : "Galil"
+
+	}
+]
